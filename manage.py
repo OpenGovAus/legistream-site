dcc2d0b3061e11bc7f0b0f33b9363cf92c9576c1
king-millez <Millez.Dev@gmail.com>
2020-12-21 01:10:49 +1100

    Better 404 page

--- manage.py
+++ manage.py
@@ -1,11 +1,32 @@
+'''
+                                .,.                                        
+                        @@(#@    @                                       
+                        @    @%/@@                                       
+                                    @                                       
+                                    @                                       
+                                #,@ @                                     
+                                %,#@@@@ @                                   
+                                %,#,@ @ @                                   
+                                %,#,@ @ @                                   
+                                &,#&@&@ @                                   
+                            #@  @*    @* /@                                
+                        @#  @,          @( .@.                            
+                ,###@@##@&####      ,#####@%##@###.                      
+OpenGov Australia*#*@.../(     @@@  *@@/     @....%@/*****************.    
+@,#,,,& %,,,% %,,,% @   *(       @  *(       @    %@*,,,@@*,,,@/(...&,/    
+@,/   % #   # #   # @   *(       @((#(       @    %@.   @@.   @/*   &,/    
+@,/   % #   # #   # @   *(%&&&&&/&&&&&,&&&&&.@    %@.   @@.   @/*   &,/    
+@,/   % #   # #   # @   *(&    @/,   @*(   @.@    %@.   @@.   @/*   &,/    
+@,/   % #   # #   # @   *(&    @/,   @*(   @.@    %@.   @@.   @/*   &,/    
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@/
+'''
+
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'legistream_site.settings')
     try:
         from django.core.management import execute_from_command_line
