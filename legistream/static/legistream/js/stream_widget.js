1ad544dd871aa779b5acba5a220de2f822bbc1e2
king-millez <millez.dev@gmail.com>
2021-01-14 14:06:17 +1100

    Change video box position

--- legistream/static/legistream/js/stream_widget.js
+++ legistream/static/legistream/js/stream_widget.js
@@ -42,7 +42,7 @@ function gen_widget(stream_title, stream_url, stream_safe, back_btn_loc)
     widget_container.style.height = '100%'
     widget_container.style.backgroundColor = 'rgba(24, 24, 24, 0.97)'
     widget_container.style.top = '0'
-    
+
     opengov_container = document.getElementById('opengov-container')
     opengov_container.style.transition = 'all 0.0s'
     opengov_container.style.zIndex = '-1'
@@ -60,7 +60,7 @@ function gen_widget(stream_title, stream_url, stream_safe, back_btn_loc)
     clicked.style.visibility = 'hidden'
     if(playing === false)
     {
-        widget_container.innerHTML = `<style>video {opacity: 1.0;} #stream-text {padding: 30px; font-size: 55pt; font-family: gilroy;} #back-btn {right: 40px; top: 52px; position: absolute;} #back-btn-img {height: 18pt; padding-right: 5pt;} #back-btn:hover {color: rgb(68, 219, 78); cursor: pointer;} #back-btn:hover #back-btn-img {padding-right: 10pt;}</style><h1 id="stream-text">${stream_title.toUpperCase()}</h1><button id="back-btn" onclick="close_widget('${stream_safe}')"><img id="back-btn-img" src="${back_btn_loc}">Back</button><div id="stream-video-container"></div>`
+        widget_container.innerHTML = `<style>video {position: absolute; bottom: 50pt;} #stream-text {padding: 30px; font-size: 55pt; font-family: gilroy;} #back-btn {right: 40px; top: 52px; position: absolute;} #back-btn-img {height: 18pt; padding-right: 5pt;} #back-btn:hover {color: rgb(68, 219, 78); cursor: pointer;} #back-btn:hover #back-btn-img {padding-right: 10pt;} #vid-${stream_safe} {margin-top: 35pt;}</style><h1 id="stream-text">${stream_title.toUpperCase()}</h1><button id="back-btn" onclick="close_widget('${stream_safe}')"><img id="back-btn-img" src="${back_btn_loc}">Back</button><div id="stream-video-container"></div>`
         document.getElementById('stream-video-container').innerHTML = `<video id="vid-${stream_safe}" class="video-js vjs-default-skin" width="800" height="450" controls><source src="${stream_url}" type="application/x-mpegURL"></video>`
         playVideo(stream_safe)
     }
