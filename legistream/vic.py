37f4071ed1517779e5b79f84f29c967d0416ba39
king-millez <Millez.Dev@gmail.com>
2021-02-16 13:29:27 +1100

    [vic] add Victorian thumbnails

--- legistream/vic.py
+++ legistream/vic.py
@@ -5,10 +5,10 @@ vic_stream = vicStream()
 vic_urls = []
 
 if(vic_stream.lower_is_live):
-    vic_urls.append({'url': vic_stream.lower_stream_url, 'title': 'Legislative Assembly', 'safe': 'legislative-assembly-vid'})
+    vic_urls.append({'url': vic_stream.lower_stream_url, 'title': 'Legislative Assembly', 'safe': 'legislative-assembly-vid', 'thumb': 'legistream/img/thumbs/vic_la.png'})
 if(vic_stream.upper_is_live):
-    vic_urls.append({'url': vic_stream.upper_stream_url, 'title': 'Legislative Council', 'safe': 'legislative-council-vid'})
+    vic_urls.append({'url': vic_stream.upper_stream_url, 'title': 'Legislative Council', 'safe': 'legislative-council-vid', 'thumbs': 'legistream/img/thumbs/vic_lc.png'})
 if(vic_stream.committee_is_live):
-    vic_urls.append({'url': vic_stream.committee_stream_url, 'title': 'Committee', 'safe': 'com-vid'})
+    vic_urls.append({'url': vic_stream.committee_stream_url, 'title': 'Committee', 'safe': 'com-vid', 'thumb': 'legistream/img/thumbs/vic_com.png'})
 
 parl_title = 'Victoria'
--- legistream/wa.py
+++ legistream/wa.py
@@ -5,8 +5,8 @@ wa_stream = waStream()
 wa_urls = []
 
 if(wa_stream.lower_is_live):
-    wa_urls.append({'url': wa_stream.lower_stream_url, 'title': 'Legislative Assembly', 'safe': 'legislative-assembly-vid'})
+    wa_urls.append({'url': wa_stream.lower_stream_url, 'title': 'Legislative Assembly', 'safe': 'legislative-assembly-vid', 'thumb': 'legistream/img/thumbs/wa_la.png'})
 if(wa_stream.upper_is_live):
-    wa_urls.append({'url': wa_stream.upper_stream_url, 'title': 'Legislative Council', 'safe': 'legislative-council-vid'})
+    wa_urls.append({'url': wa_stream.upper_stream_url, 'title': 'Legislative Council', 'safe': 'legislative-council-vid', 'thumb': 'wa_lc.png'})
 
 parl_title = 'Western Australia'
