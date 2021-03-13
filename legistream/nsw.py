8cbde22944e6dcc7e070eb790a08d124903c550b
king-millez <Millez.Dev@gmail.com>
2021-03-13 16:02:58 +1100

    [nsw] committe -> committee

--- legistream/nsw.py
+++ legistream/nsw.py
@@ -9,7 +9,7 @@ if(nsw_stream.lower_is_live):
 if(nsw_stream.upper_is_live):
     nsw_urls.append({'url': nsw_stream.upper_stream_url, 'title': 'Legislative Council', 'safe': 'legislative-council-vid', 'thumb': 'legistream/img/thumbs/nsw_lc.webp'})
 if(nsw_stream.committee_is_live):
-    nsw_urls.append({'url': nsw_stream.committe_stream_url, 'title': 'Committee', 'safe': 'com-vid', 'thumb': 'legistream/img/thumbs/nsw_com.webp'})
+    nsw_urls.append({'url': nsw_stream.committee_stream_url, 'title': 'Committee', 'safe': 'com-vid', 'thumb': 'legistream/img/thumbs/nsw_com.webp'})
 if(nsw_stream.jubilee_is_live):
     nsw_urls.append({'url': nsw_stream.jubilee_stream_url, 'title': 'Jubilee', 'safe': 'jubilee-vid', 'thumb': 'legistream/img/thumbs/nsw_jubilee.webp'})
 
