94855b088c0b6b20f5e3f2dfdfa815868a593d24
king-millez <Millez.Dev@gmail.com>
2021-08-19 19:21:24 +1000

    [mobile] stream widget styling for mobile

--- legistream/static/legistream/js/stream_widget.js
+++ legistream/static/legistream/js/stream_widget.js
@@ -1,4 +1,5 @@
 var player = '';
+let isMobile = false;
 
 
 var playVideo = (stream_safe) => {
@@ -30,6 +31,9 @@ var close_widget = (clicked_thumb) => {
 
 
 var gen_widget = (stream_title, stream_url, stream_safe, back_btn_loc) => {
+    if($('.stream-thumb').first().width() == 250) {
+        isMobile = true;
+    };
     styleDict = {
         'transition': 'all 0.0s',
         'zIndex': '-1'
@@ -49,45 +53,91 @@ var gen_widget = (stream_title, stream_url, stream_safe, back_btn_loc) => {
         'height': '100%',
         'backgroundColor': 'rgba(24, 24, 24, 0.97)',
         'top': '0'
-    }).html(
-        `<style>
-            video {
-                position: absolute;
-                bottom: 50pt;
-            }
-            #stream-text {
-                padding: 30px;
-                font-size: 40px;
-                font-family:gilroy;
-                width: 80%;
-            }
-            #back-btn {
-                right: 40px;
-                top: 52px;
-                position: absolute;
-            }
-            #back-btn-img {
-                height: 11pt;
-                padding-right: 5pt;
-            }
-            #back-btn:hover {
-                color: rgb(68, 219, 78);
-                cursor: pointer;
-            }
-            #back-btn:hover #back-btn-img {
-                padding-right: 10pt;
-            }
-            #vid-${stream_safe} {
-                margin-top: 35pt;
-            }
-        </style>
-        <h1 id="stream-text">${stream_title.toUpperCase()}</h1>
-        <button id="back-btn" onclick="close_widget('${stream_safe}')">
-            <img id="back-btn-img" src="${back_btn_loc}">
-            Back
-        </button>
-        <div id="stream-video-container"></div>`
-    );
+    })
+    if(!isMobile) {
+        $('#stream-widget-container').html(
+            `<style>
+                video {
+                    position: absolute;
+                    bottom: 50pt;
+                }
+                #stream-text {
+                    padding: 30px;
+                    font-size: 40px;
+                    font-family:gilroy;
+                    width: 80%;
+                }
+                #back-btn {
+                    right: 40px;
+                    top: 52px;
+                    position: absolute;
+                }
+                #back-btn-img {
+                    height: 11pt;
+                    padding-right: 5pt;
+                }
+                #back-btn:hover {
+                    color: rgb(68, 219, 78);
+                    cursor: pointer;
+                }
+                #back-btn:hover #back-btn-img {
+                    padding-right: 10pt;
+                }
+                #vid-${stream_safe} {
+                    margin-top: 35pt;
+                }
+            </style>
+            <h1 id="stream-text">${stream_title.toUpperCase()}</h1>
+            <button id="back-btn" onclick="close_widget('${stream_safe}')">
+                <img id="back-btn-img" src="${back_btn_loc}">
+                Back
+            </button>
+            <div id="stream-video-container"></div>`
+        );
+    } else {
+        $('#stream-widget-container').html(
+            `<style>
+                video {
+                    position: absolute;
+                    bottom: 50pt;
+                }
+                #stream-text {
+                    padding: 30px;
+                    font-size: 28px;
+                    font-family:gilroy;
+                    width: 55%;
+                }
+                #back-btn {
+                    right: 40px;
+                    top: 52px;
+                    position: absolute;
+                }
+                #back-btn-img {
+                    height: 11pt;
+                    padding-right: 5pt;
+                }
+                #back-btn:hover {
+                    color: rgb(68, 219, 78);
+                    cursor: pointer;
+                }
+                #back-btn:hover #back-btn-img {
+                    padding-right: 10pt;
+                }
+                #vid-${stream_safe} {
+                    margin-top: 95pt;
+                    width: 355px;
+                    height: 200px;
+                }
+            </style>
+            <h1 id="stream-text">${stream_title.toUpperCase()}</h1>
+            <button id="back-btn" onclick="close_widget('${stream_safe}')">
+                <img id="back-btn-img" src="${back_btn_loc}">
+                Back
+            </button>
+            <div id="stream-video-container"></div>`
+        );
+    };
+
     $('#stream-video-container').html(
         `<video id="vid-${stream_safe}" class="video-js vjs-default-skin" width="800" height="450" controls>
             <source src="${stream_url.replace('http://', 'https://')}" type="application/x-mpegURL">
